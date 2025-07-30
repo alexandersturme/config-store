fe192062fddbb6f9b6adaf900fd819f8860f1523
Alexander Sturme <sasha_st@inbox.lv>
2025-07-30 17:08:42 +0300

    feat: update Docker and application configuration, implement CRUD routes for key-value storage

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const express = require('express');
+const db = require('./db');
+const apiRouter = require('./routes');
+
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!');
+  res.send('Hello world!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-module.exports = app;
+db.authenticate()
+  .then(() => {
+    console.log('Connected to PostgreSQL.');
+    return db.sync();
+  })
+  .then(() => {
+    console.log('Database synchronized, starting server.');
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Unable to connect to DB');
+    console.error(err);
+  });
